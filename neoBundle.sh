d9033028f5459f1c799e82839f0e054af2af1b57
Naoki <hnron.reg@gmail.com>
2015-08-19 22:09:56 +0900

    neoBundle.shにpyenvとかvimprocとかいろいろ追加

--- neoBundle.sh
+++ neoBundle.sh
@@ -2,5 +2,7 @@
 
 sudo pacman -S ctags python-pip npm
 
+cd ~/            && git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+cd ~/.vim/bundle && git clone https://github.com/Shougo/vimproc.vim
 cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
 
