ad98d9478438fc2f4ca3b49fe1b8e67af5b2e3aa
Naoki <hnron.reg@gmail.com>
2015-08-22 08:51:27 +0900

    neobundleのvimproc消した

--- neoBundle.sh
+++ neoBundle.sh
@@ -3,6 +3,5 @@
 sudo pacman -S ctags python-pip npm
 
 cd ~/            && git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-cd ~/.vim/bundle && git clone https://github.com/Shougo/vimproc.vim
 cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
 
