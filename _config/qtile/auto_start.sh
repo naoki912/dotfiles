460f2cde974012a6106d72077c23291c7d8e85c4
Naoki <hnron.reg@gmail.com>
2015-12-15 14:06:53 +0900

    qtileのconfigを追加

---
+++ _config/qtile/auto_start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+feh --bg-scale $HOME/Pictures/background_3.png &
+pa-applet &
+shutter &
+pidgin &
+ibus-daemon -drx &
+google-musicmanager &
+owncloud &
+slack &
+twmnd &
+xmodmap ~/.xmodmaprc
