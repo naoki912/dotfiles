6de8d9e806ac6dd0c300b7fa1cc80da82eaa0d45
Naoki <hnron.reg@gmail.com>
2016-07-07 19:15:53 +0900

    [change] neoBundle.shとsetup.shを統合

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-sudo pacman -S archey3
+sudo -E pacman -S ctags python-pip npm archey3
 
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+cd ~/            && git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
