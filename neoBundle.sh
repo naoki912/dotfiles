050c04c8823c50d14782e50b52d56c7c9cab0a6d
Naoki <hnron.reg@gmail.com>
2016-07-07 19:15:53 +0900

    [change] neoBundle.shとsetup.shを統合

--- neoBundle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-sudo pacman -S ctags python-pip npm
-
-cd ~/            && git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
-
