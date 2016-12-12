ba36aa5f21ddd43ca0e1e658e446275dc05e2f3c
Naoki Kanazawa <nk.naoki912@gmail.com>
2016-12-12 20:36:05 +0900

    [update] setup.sh: pyenv, virtualenv

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 sudo -E pacman -S ctags python-pip npm archey3
 
-cd ~/            && git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
