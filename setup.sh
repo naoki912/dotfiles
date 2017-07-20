79d2190d5303435d12a128ec3b7ea3e011ff6c73
Naoki Kanazawa <kanazawa@naoki912.net>
2017-07-20 18:50:00 +0900

    [Add] setup.sh にTmux周りの設定を入れた

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
-sudo -E pacman -S ctags python-pip npm archey3
+sudo -E pacman -S --noconfirm ctags python-pip npm archey3
 
+# pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# vim
 cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim
+
+# tmux
+sudo -E pacman -S --noconfirm xsel
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
