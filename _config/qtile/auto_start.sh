55faa33feb73225866408496d97fe784d54e0776
Naoki Kanazawa <nk.naoki912@gmail.com>
2018-06-04 17:54:45 +0900

    ThinkPadのNatural Scrollingの設定を入れた

--- _config/qtile/auto_start.sh
+++ _config/qtile/auto_start.sh
@@ -28,3 +28,6 @@ xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
+
+# ThinkPad X280 Natural Scrolling
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
