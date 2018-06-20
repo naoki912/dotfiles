8d46a3d919b1e7c33770683664b368169f803a48
Naoki Kanazawa <kanazawa@naoki912.net>
2018-06-20 15:56:48 +0900

    dmenuからrofiに変更

--- _config/qtile/config.py
+++ _config/qtile/config.py
@@ -7,7 +7,19 @@ from libqtile import layout, bar, widget, hook
 
 
 class Commands(object):
-    dmenu = 'dmenu_run'
+    rofi = """rofi -show run -modi run -location 1 -width 100 \
+            -lines 2 -line-margin 0 -line-padding 1 \
+            -separator-style none -font "mono 10" -columns 9 -bw 0 \
+            -disable-history \
+            -hide-scrollbar \
+            -color-window "#222222, #222222, #b1b4b3" \
+            -color-normal "#222222, #b1b4b3, #222222, #005577, #b1b4b3" \
+            -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
+            -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" \
+            -kb-row-select "Tab" -kb-row-tab ""
+    """
+    dmenu = rofi
+    # dmenu = 'dmenu_run'
     # terminal = 'terminator'
     # terminal = 'termite'
     terminal = 'termite --title=tmux -e tmux'
