661a5a38e7ceef3891883092fd041de2fc9d244b
Naoki Kanazawa <kanazawa@naoki912.net>
2018-07-02 22:59:22 +0900

    qtileでBspを使用できるように変更

--- _config/qtile/config.py
+++ _config/qtile/config.py
@@ -24,41 +24,60 @@ keys = [
     # Window manager controls
     Key([mod, 'control', 'shift'], 'r', lazy.restart()),
     # Key([mod, 'control', 'shift'], 'q', lazy.shutdown()),
-    Key([mod], 'Return', lazy.spawn(Commands.terminal)),
-    Key([mod, 'shift'], 'q', lazy.window.kill()),
 
+    # Switch window focus to other pane(s) of stack
+    # 以下のkeymapはBspだと動作しない
+    Key([mod], 'space', lazy.layout.next()),
     Key([mod], 'o', lazy.layout.next()),
-    Key([mod, 'shift'], 'space', lazy.layout.flip()),
-    # qtileのトップページに載ってるけどドキュメントに無い
-    # Key([mod], 'Left', lazy.screen.prevgroup()),
-    # Key([mod], 'Right', lazy.screen.nextgroup()),
 
-    # Layout modification
+    # Active window を Floating layout に変更
     Key([mod, 'control'], 'space', lazy.window.toggle_floating()),
 
-    # Switch between windows in current stack pane
+    # Active window を移動
     Key([mod], 'h', lazy.layout.left()),
     Key([mod], 'j', lazy.layout.down()),
     Key([mod], 'k', lazy.layout.up()),
     Key([mod], 'l', lazy.layout.right()),
 
-    # Move windows up or down in current stack
-    Key([mod, 'shift'], 'h', lazy.layout.swap_left()),
-    Key([mod, 'shift'], 'l', lazy.layout.swap_right()),
+    # Windowを *入れ替える*
+    #     全体のレイアウト/配置を変更せずにWindowの中身を入れ替える
+    # Monadtailを使用する/共存させる
+    #     layout切替時のhookで、現在のレイアウトを読んで都度keyをupdateしたほうが良さそう
+    #     monadtailを使用する場合は以下に書き換えること
+    #         shuffle_left() -> swap_left()
+    #         shuffle_right() -> swap_right()
+    #     monadtailの場合 shuffle_left() では操作を受け付けてくれない(何も起きない)
+    #     bspの場合 shuffle_left() でないと操作を受け付けてくれない
+    #Key([mod, 'shift'], 'h', lazy.layout.swap_left()),
+    #Key([mod, 'shift'], 'l', lazy.layout.swap_right()),
+    Key([mod, 'shift'], 'h', lazy.layout.shuffle_left()),
     Key([mod, 'shift'], 'j', lazy.layout.shuffle_down()),
     Key([mod, 'shift'], 'k', lazy.layout.shuffle_up()),
+    Key([mod, 'shift'], 'l', lazy.layout.shuffle_right()),
 
-    # Switch window focus to other pane(s) of stack
-    Key([mod], 'space', lazy.layout.next()),
+    # Windowの *位置を交換*
+    # サイズはそのままで、交換先が複数のWindowで構成されている場合はサイズを変更せずにまるごと位置を入れ替える
+    # ex) 以下のWindowの配置で、4をActiveにした状態でflip_up()したときの挙動
+    # | 1 |2|  3  |     ->      |    4      |
+    # |    4      |   flip_up   | 1 |2|  3  |
+    Key([mod, 'control', 'shift'], 'j', lazy.layout.flip_down()),
+    Key([mod, 'control', 'shift'], 'k', lazy.layout.flip_up()),
+    Key([mod, 'control', 'shift'], 'h', lazy.layout.flip_left()),
+    Key([mod, 'control', 'shift'], 'l', lazy.layout.flip_right()),
 
-    # Toggle between different layouts as defined below
+    # Window size
+    # Bsp Layoutでは使用しない
+    #Key([mod], 'i', lazy.layout.grow()),
+    #Key([mod], 'm', lazy.layout.shrink()),
+    #Key([mod], 'n', lazy.layout.normalize()),
+    #Key([mod], 'u', lazy.layout.maximize()),
+    Key([mod, 'control'], 'j', lazy.layout.grow_down()),
+    Key([mod, 'control'], 'k', lazy.layout.grow_up()),
+    Key([mod, 'control'], 'h', lazy.layout.grow_left()),
+    Key([mod, 'control'], 'l', lazy.layout.grow_right()),
+
+    # 次のLayoutに変更
     Key([mod], 'Tab', lazy.next_layout()),
-
-    # ウィンドウサイズ
-    Key([mod], 'i', lazy.layout.grow()),
-    Key([mod], 'm', lazy.layout.shrink()),
-    Key([mod], 'n', lazy.layout.normalize()),
-    Key([mod], 'u', lazy.layout.maximize()),
 
     # アプリケーション起動
     Key([mod], 'w', lazy.spawn(Commands.rofi_window)),
@@ -66,6 +85,10 @@ keys = [
     Key([mod], 's', lazy.spawn(Commands.rofi_windowcd)),
     Key([mod], 'd', lazy.spawn(Commands.dmenu)),
     Key([mod], 'f', lazy.spawn(Commands.nm_dmenu)),
+    Key([mod], 'Return', lazy.spawn(Commands.terminal)),
+
+    # Window kill
+    Key([mod, 'shift'], 'q', lazy.window.kill()),
 ]
 
 # Mouse bindings and options
@@ -123,7 +146,18 @@ dgroups_app_rules = []
 
 layouts = [
     layout.Max(),
-    layout.MonadTall(),
+    # layout.MonadTall(),
+    layout.Bsp(
+        border_focus='#0000ff', # Border colour for the focused window.
+        border_normal='#000000', # Border colour for un-focused windows.
+        border_width=3, # Border width.
+        fair=True, # New clients are inserted in the shortest branch.
+        grow_amount=10, # Amount by which to grow a window/column.
+        lower_right=True, # New client occupies lower or right subspace.
+        margin=3, # Margin of the layout
+        name='bsp', # Name of this layout.
+        ratio=1.6, # Width/height ratio that defines the partition direction.
+    ),
 ]
 
 floating_layout = layout.Floating()
