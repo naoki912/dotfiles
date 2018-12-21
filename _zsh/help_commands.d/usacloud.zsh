c39a8a453bab32cdaa64de5e0f1b0daba9175fe7
Naoki Kanazawa <kanazawa@naoki912.net>
2018-12-21 12:01:57 +0900

    help_commands.d

---
+++ _zsh/help_commands.d/usacloud.zsh
@@ -0,0 +1,61 @@
+alias h-usacloud-server-build='cat --bg=dark << EOF
+usacloud server build \\
+    --core=1 \\
+    --memory=1 \\
+    --disk-mode=create \\
+    --os-type=ubuntu \\
+    --disk-size=20 \\
+    --network-mode=switch \\
+    --switch-id= {{SWITCH_ID}} \\
+    --hostname= {{HOSTNAME}} \\
+    --password= {{PASSWORD}} \\
+    --disable-pw-auth=true \\
+    --ip=10.0.0.1 \\
+    --nw-masklen=24 \\
+    --gateway=10.0.0.254 \\
+    --ssh-key-mode=id \\
+    --ssh-key-ids= {{SSH_KEY_ids}} \\
+    --name= {{NAME}} \\
+    --description= {{DESCRIPTION}} \\
+    --tags= {{TAGS}} \\
+    -y
+
+usacloud server build --core=1 --memory=1 --disk-mode=create --os-type=ubuntu --disk-size=20 --network-mode=switch --switch-id= {{SWITCH_ID}} --hostname= {{HOSTNAME}} --password= {{PASSWORD}} --disable-pw-auth=true --ip=10.0.0.1 --nw-masklen=24 --gateway=10.0.0.254 --ssh-key-mode=id --ssh-key-ids= {{SSH_KEY_ids}} --name= {{NAME}} --description= {{DESCRIPTION}} --tags= {{TAGS}} -y
+
+- --network-mode
+  - shared
+    switch
+    disconnect
+    none
+- --password
+  - \${SAKURACLOUD_SERVER_PASSWORD}
+EOF
+'
+
+alias h-usacloud-server-build-template='cat --bg=dark << EOF
+usacloud server build -y --param-template {
+    "core": 1,
+    "memory": 1,
+    "disk-mode": "create",
+    "os-type": "ubuntu",
+    "disk-size": 20,
+    "network-mode": "switch",
+    "switch-id": "{{SWITCH_ID}}",
+    "hostname": "{{HOSTNAME}}",
+    "password": "{{PASSWORD}}",
+    "disable-pw-auth": "true",
+    "ip": "10.0.0.1",
+    "nw-masklen": 24,
+    "gateway": "10.0.0.254",
+    "ssh-key-mode": "id",
+    "ssh-key-ids": "{{SSH_KEY_ids}}",
+    "name": "{{NAME}}",
+    "description": "{{DESCRIPTION}}",
+    "tags": "{{TAGS}}"
+}
+
+or
+
+--param-template-file
+EOF
+'
