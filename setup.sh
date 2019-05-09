88b575304fc2bc23a7d976805aa46f1447e83b6f
Naoki Kanazawa <kanazawa@naoki912.net>
2019-05-09 12:39:59 +0900

    Add rbenv

--- setup.sh
+++ setup.sh
@@ -12,6 +12,12 @@ sudo -E pacman -S --noconfirm ctags python-pip npm archey3
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
+#--
+# rbenv
+#--
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 #--
 # vim
