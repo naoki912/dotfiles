c329d44f0ed66ea93cb0083383b436f2df4d80ba
Naoki Kanazawa <nk.naoki912@gmail.com>
2021-11-17 17:54:09 +0900

    brew

---
+++ ansible/mac.sh
@@ -0,0 +1,53 @@
+#--
+# brew
+#--
+
+# gnu
+brew install binutils
+brew install coreutils
+brew install diffutils
+brew install findutils
+brew install moreutils
+brew install gawk
+brew install gnu-sed
+brew install gnu-tar
+brew install gnutls
+brew install grep
+brew install gzip
+brew install screen
+brew install wdiff
+brew install wget
+brew install ed
+brew install gnu-indent
+brew install gnu-which
+brew install gnu-time
+brew install gnu-getopt
+brew install gnupg
+
+# docker, kubernetes
+brew install docker
+brew install docker-compose
+brew install kind
+brew install kubebuilder
+brew install kubectx
+brew install kubernetes-cli
+brew install stern
+
+# openstack
+brew install openstackclient
+
+# command line tools
+brew install fzf
+brew install jq
+brew install tmux
+brew install neovim
+
+# language
+brew install go
+brew install python
+
+#==> Casks
+brew install --cask docker
+brew install --cask iterm2
+brew install --cask visual-studio-code
+
