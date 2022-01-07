f812c57c6f09d8ba78514b35ac798e918cf91e6e
Naoki Kanazawa <nk.naoki912@gmail.com>
2022-01-07 22:30:19 +0900

    mac用ansible

--- ansible/README.md
+++ ansible/README.md
@@ -9,3 +9,16 @@ ansible-playbook arch_base.yml --ask-become-pass
 ansible-playbook arch_gui.yml --ask-become-pass
 ```
 
+macの場合
+```sh
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv activate ansible
+
+pyenv exec ansible-playbook dotfile.yml --ask-become-pass
+pyenv exec ansible-playbook arch_base.yml --ask-become-pass
+pyenv exec ansible-playbook arch_gui.yml --ask-become-pass
+```
+
--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/bash
 
-set -uvx
+set -vx
 
 
-function install_ansible_on_mac() {
+function install_ansible_on_mac_with_brew() {
     if ! type brew > /dev/null 2>&1; then
         bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     brew install ansible
+}
+
+
+function install_ansible() {
+    if [ -d ${HOME}/.pyenv ]; then
+        git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+        git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    fi
+
+    export PYENV_ROOT="${HOME}/.pyenv"
+    export PATH=${PYENV_ROOT}/bin:${PATH}
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+
+    pyenv install 3.10.1
+    pyenv virtualenv 3.10.1 ansible
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+    pyenv activate ansible
+    pyenv exec pip install ansible
+
+    echo '=========='
+    echo 'eval "$(pyenv init --path)"'
+    echo 'eval "$(pyenv init -)"'
+    echo 'eval "$(pyenv virtualenv-init -)"'
+    echo 'pyenv exec ansible-playbook ...'
+    echo '=========='
 }
 
 
