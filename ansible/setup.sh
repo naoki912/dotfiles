507f071b622cb94e6ab586e6a62a0d0fdb5b1987
Naoki Kanazawa <nk.naoki912@gmail.com>
2022-01-07 21:57:08 +0900

    ansible/setup.sh 条件分岐が間違っていたので修正

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -4,7 +4,7 @@ set -uvx
 
 
 function install_ansible_on_mac() {
-    if type brew > /dev/null 2>&1; then
+    if ! type brew > /dev/null 2>&1; then
         bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     brew install ansible
@@ -16,7 +16,7 @@ function install_ansible_on_linux() {
 }
 
 
-if type ansible-playbook > /dev/null 2>&1; then
+if ! type ansible-playbook > /dev/null 2>&1; then
     if [[ "$(uname -s)" == "Darwin" ]]; then
         install_ansible_on_mac
     elif [[ "$(uname -s)" == "Linux" ]]; then
